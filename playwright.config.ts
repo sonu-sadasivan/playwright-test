a5f64c79cd4db2d61dce99b9e4ad6ca54210cb87
sonu-sadasivan <sonu.sadasivan@gmail.com>
2025-10-06 14:47:39 +0100

    Added files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+
 export default defineConfig({
   testDir: './tests',                
   timeout: 30 * 1000,                
   retries: 1,                        
   use: {
     headless: false,                
-    baseURL: 'https://playwright.dev',  
+    baseURL: process.env.BASE_URL,
     screenshot: 'on',                // Capture screenshots on failure
     video: 'retain-on-failure',      // Record video only when failed
   },
