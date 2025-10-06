24e70b512819bec1d400567ab558b8886b0ca053
sonu-sadasivan <sonu.sadasivan@gmail.com>
2025-10-06 13:50:13 +0100

    added files

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',                
+  timeout: 30 * 1000,                
+  retries: 1,                        
+  use: {
+    headless: false,                
+    baseURL: 'https://example.com',  
+    screenshot: 'on',                // Capture screenshots on failure
+    video: 'retain-on-failure',      // Record video only when failed
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
