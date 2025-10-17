da9f15cc8d1bf573476d64e68de1575febf0b8f5
sonu-sadasivan <50476844+sonu-sadasivan@users.noreply.github.com>
2025-10-17 20:30:53 +0100

    Update code-change.ts

--- code-change.ts
+++ code-change.ts
@@ -1,3 +1,20 @@
   const pseudoText2 = await this.page.locator('#panel_center .intro p').evaluate((el) => {
       return window.getComputedStyle(el, '::after').getPropertyValue('content');
         });
+
+
+  static readonly PAGE_HEADING_SELECTOR = '#panel_center .intro p';
+  static readonly PAGE_SUB_HEADING_LABEL_SELECTOR = '.attrEntry label[for=PWResetUserMessage1]';
+  static readonly EMAIL_INPUT_SELECTOR = 'li.TextBox.email>div>input';
+  static readonly NEXT_BUTTON_SELECTOR = 'button.sendCode';
+
+  static readonly PAGE_HEADING_TEXT = 'CREATE AN ACCOUNT';
+  static readonly PAGE_SUB_HEADING_TEXT = 'Enter your email to sign up';
+  static readonly EMAIL_INPUT_TEXT = 'Email address';
+  static readonly NEXT_BUTTON_TEXT = 'Next';
+
+
+  readonly pageHeading: Locator;
+  readonly pageSubHeadingLabel: Locator;
+  readonly emailInput: Locator;
+  readonly nextButton: Locator;
